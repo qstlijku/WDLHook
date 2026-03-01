9e3fb182b090ceb4d06a79a60bf45a8578b98ab1
qstlijku <63124237+qstlijku@users.noreply.github.com>
2026-02-28 23:48:20 -0800

    Created separate ChunkReader class

---
+++ ChunkReader.h
@@ -0,0 +1,47 @@
+#pragma once
+#include "Windows.h"
+#include "ext/minhook/minhook.h"
+#include "glm/glm.hpp"
+#include <glm/gtc/quaternion.hpp>
+
+class ChunkReader
+{
+public:
+	struct ChunkStream
+	{
+		const unsigned __int8* base;
+		unsigned int bitPosition;
+	};
+
+	struct ChunkStreamReaderBase
+	{
+		ChunkStream bitstream;
+		unsigned int currentDynamicDatum;
+		unsigned int numDynamicData;
+		unsigned int numFramesInThisChunk;
+		unsigned int currentBitPosition;
+		unsigned int startOfNextDatum;
+		unsigned int currentNumInterpolantBits;
+		unsigned int constFlags;
+	};
+
+	__declspec(align(16)) struct ChunkStreamReader : ChunkStreamReaderBase // quat
+	{
+		unsigned int signBits;
+	};
+
+	struct CompressedStreamReader
+	{
+		ChunkStreamReader firstChunkReader;
+		ChunkStreamReader secondChunkReader;
+		unsigned int firstFrameToRead;
+		unsigned int secondFrameToRead;
+	};
+
+	typedef void(*ReadFrameData_t)(ChunkStreamReader*, char, int, float*, int, float*);
+	typedef void(*ExtractAnyFramePair_t)(ChunkStreamReader*, int, float*, int, float*);
+	typedef void(*ReadTwoValues_t)(CompressedStreamReader*, float*, float*, int);
+	typedef uintptr_t(*GetJointRotations_t)(__int64, __int64, __int64, __int64, __int64, int, __int64, int, __int64, bool);
+
+	void printChunkReaderState(ChunkStreamReader* a1);
+};
--- Misc.h
+++ Misc.h
@@ -7,47 +7,11 @@
 class Misc
 {
 public:
-	struct ChunkStream
-	{
-		const unsigned __int8* base;
-		unsigned int bitPosition;
-	};
-
-	struct ChunkStreamReaderBase
-	{
-		ChunkStream bitstream;
-		unsigned int currentDynamicDatum;
-		unsigned int numDynamicData;
-		unsigned int numFramesInThisChunk;
-		unsigned int currentBitPosition;
-		unsigned int startOfNextDatum;
-		unsigned int currentNumInterpolantBits;
-		unsigned int constFlags;
-	};
-
-	__declspec(align(16)) struct ChunkStreamReader : ChunkStreamReaderBase // quat
-	{
-		unsigned int signBits;
-	};
-
-	struct CompressedStreamReader
-	{
-		ChunkStreamReader firstChunkReader;
-		ChunkStreamReader secondChunkReader;
-		unsigned int firstFrameToRead;
-		unsigned int secondFrameToRead;
-	};
-
 	typedef unsigned long EntityId;
 	typedef uintptr_t(*FileOpen_t)(void*, const char*, uintptr_t);
 	typedef uintptr_t(*HandleInput_t)(void*, __int64);
 	typedef uintptr_t(*Takedown_t)(void*);
 	typedef int(*TakedownResult_t)(__int64);
-
-	typedef void(*ReadFrameData_t)(ChunkStreamReader *, char, int, float *, int, float *);
-	typedef void(*ExtractAnyFramePair_t)(ChunkStreamReader*, int, float*, int, float*);
-	typedef void(*ReadTwoValues_t)(CompressedStreamReader*, float*, float*, int);
-	typedef uintptr_t(*GetJointRotations_t)(__int64, __int64, __int64, __int64, __int64, int, __int64, int, __int64, bool);
 
 	typedef void(*CreateResource_t)(void *, void *, __int64);
 	typedef void(*SetLethal_t)(__int64, bool);
@@ -57,5 +21,3 @@ public:
 	static void Initialize();
 
 };
-
-void printChunkReaderState(Misc::ChunkStreamReader* a1);
